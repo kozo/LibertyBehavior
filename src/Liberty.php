11c43e7c2421ac49ba7864773fe42bbd0f7a789a
Junkins <wanwansiba3@gmail.com>
2016-06-23 13:48:41 +0900

    .sqlだけでなく他の拡張子にも対応できるように拡張

--- src/Liberty.php
+++ src/Liberty.php
@@ -2,28 +2,41 @@
 
 namespace Liberty;
 
+use Cake\Network\Exception\InternalErrorException;
+use Cake\Utility\Inflector;
 use Cake\View\ViewBuilder;
 
 class Liberty
 {
     /**
-     * get the SQL
+     * get the result of rendering
      *
      * @access public
-     * @author sakuragawa
+     * @author ito
      */
-    public static function sql($fileName, array $data = [])
+    public static function __callStatic ($name, $arguments)
     {
+        if (
+            (!isset($name) || !is_string($name)) ||
+            (!isset($arguments[0]) || !is_string($arguments[0]))
+        ) {
+            throw new InternalErrorException();
+        }
+
+        $fileName = $arguments[0];
+        $data     = (isset($arguments[1]) && is_array($arguments[1]))? $arguments[1]: [];
+
         $builder = new ViewBuilder();
         $view = $builder
             ->className('Cake\View\View')
-            ->templatePath('Sql')
+            ->templatePath(Inflector::camelize($name))
             ->layout(false)
             ->build();
 
-        $view->_ext = '.sql';
+        $view->_ext = '.' . $name;
         $view->viewVars = $data;
 
         return $view->render($fileName);
     }
 }
+
